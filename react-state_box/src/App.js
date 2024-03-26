77cc2a2335e73f11389681b140d99cd6dfd9affb
Eric <Karstenswobi@gmail.com>
2024-03-26 15:13:27 +0100

    react-state-box

---
+++ react-state_box/src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick() {
+    setIsActive(!isActive);
+    console.log(isActive);
+  }
+
+  return (
+    <main>
+      <div className={`box ${isActive ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        Activate
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
+    </main>
+  );
+}
